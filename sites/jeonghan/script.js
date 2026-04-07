fc832acba74c38f9b766d9665f073e7087c4a5d0
박지수 <trleresoo@gmail.com>
2026-04-07 12:18:11 +0900

    refactor: 댓글 기능 잠시 치우기, 후에 ui 변경할 예정

--- sites/jeonghan/script.js
+++ sites/jeonghan/script.js
@@ -1,10 +1,6 @@
 import { initCountdown, getDDayString } from '../../packages/shared/timer.js';
-import { CommentUI } from '../../packages/shared/commentUI.js';
 
 const TARGET_DATE = "June 25, 2026 00:00:00";
 
 // 1. 카운트다운 초기화
 initCountdown(TARGET_DATE, "clock-jeonghan");
-
-// 2. 댓글 시스템 초기화 (멤버 ID: 'jeonghan')
-CommentUI.init('jeonghan', () => getDDayString(TARGET_DATE));
--- sites/woozi/script.js
+++ sites/woozi/script.js
@@ -1,10 +1,6 @@
 import { initCountdown, getDDayString } from '../../packages/shared/timer.js';
-import { CommentUI } from '../../packages/shared/commentUI.js';
 
 const TARGET_DATE = "March 14, 2027 00:00:00";
 
 // 1. 카운트다운 초기화
 initCountdown(TARGET_DATE, "clock-woozi");
-
-// 2. 댓글 시스템 초기화 (멤버 ID: 'woozi')
-CommentUI.init('woozi', () => getDDayString(TARGET_DATE));
